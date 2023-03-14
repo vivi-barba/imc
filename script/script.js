c16943f8acd94a48e2e2f5c461b5f6fc314f935f
vivi-barba <vitoriabbarbosa31@gmail.com>
2023-03-14 18:41:43 -0300

    feat: manipulando função

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function pulaLinha() {
+        document.write("<br>");
+      }
+
+      function mostra(frase) {
+        document.write(frase);
+        pulaLinha();
+      }
+
+      function calculoImc(altura, peso) {
+        imc = peso / (altura * altura);
+        mostra("Seu imc é " + Math.round(imc));
+      }
+      // var peso = 61;
+      // var altura = 1.71;
+      // var imc = peso / (altura * altura);
+
+      calculoImc(1.71, 61);
